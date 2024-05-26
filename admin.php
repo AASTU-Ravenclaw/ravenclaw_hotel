4d8f1fbfeb99869a3001d4fb645f9e0ca66947c6
kirubel midru <kira200336@gmail.com>
2024-05-27 00:23:06 +0300

    refresh page css add a logout function separate the db connectino remake db to include a confirmation code

--- admin.php
+++ admin.php
@@ -25,7 +25,7 @@ if(!isset($_SESSION['username'])) {
     <script src="js/tabbing.js"></script>
 
     <header>
-      <div class="header-nav">
+      <div>
         <div>
           <img src="img/main.avif" width="50px" alt="logo">
         </div>
