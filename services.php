d53799071bd1152affffba8a6f1639af07f4ef5e
Meron Getaneh <merongetaneh.sel9@gmail.com>
2024-06-14 08:23:24 +0300

    c

--- services.php
+++ services.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hotels by RavenClaw</title>
     <link rel="stylesheet" href="css/index_style.css">
-
+    <link rel="stylesheet" href="css/services.css">
     <link rel="icon" href="favicon.ico" sizes="any">
     <link rel="icon" href="icon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="icon.png">
